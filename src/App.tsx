9301599b696a390c61770cdc9e8bf3d4816078b0
Tenderly__ <olevnao31@gmail.com>
2026-04-02 20:17:30 +0300

    feat: compose app state and connect product list with details view

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
-import './App.css'
+import { useState } from "react";
+import { ProductDetails } from "./components/ProductDetails";
+import { ProductList } from "./components/ProductList";
+import { initialProducts } from "./data/products";
+import type { IProduct } from "./types/product";
 
-function App() {}
+function App() {
+  const [state] = useState<IProduct[]>(initialProducts);
+  const [selectedProductId, setSelectedProductId] = useState<string | null>(
+    null,
+  );
+  function handleSelect(id: string): void {
+    setSelectedProductId(id);
+  }
 
-export default App
+  const selectedProduct = state.find(
+    (product) => product.id === selectedProductId,
+  );
+
+  return (
+    <>
+      <ProductList
+        product={state}
+        selectedProductId={selectedProductId}
+        onSelect={handleSelect}
+      />
+
+      <ProductDetails product={selectedProduct} />
+    </>
+  );
+}
+
+export default App;
