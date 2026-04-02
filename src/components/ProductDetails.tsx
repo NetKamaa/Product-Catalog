3b4d5a51a238d9fe1944a50bd3bbc97aac6c24e1
Tenderly__ <olevnao31@gmail.com>
2026-04-02 20:14:17 +0300

    feat: add component ProductDetails

---
+++ src/components/ProductDetails.tsx
@@ -0,0 +1,19 @@
+import type { IProduct } from "../types/product";
+
+interface IProductDetails {
+  product: IProduct | undefined;
+}
+
+export function ProductDetails({ product }: IProductDetails) {
+  return (
+    <>
+      <div>
+        <h2>{product?.title}</h2>
+        <h4>{product?.price} $</h4>
+        <h4>{product?.category}</h4>
+        <h4>{product?.inStock}</h4>
+        <h3>{product?.description}</h3>
+      </div>
+    </>
+  );
+}
